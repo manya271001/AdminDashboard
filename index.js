dde5b894664ed4a87b1ccf2ef82b91947eb91897
Manya Sharma <manya271001@gmail.com>
2024-11-24 22:28:17 +0530

    navnar for dashboard

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+
 function signup(){
     let obj={
-    receivedemail:document.querySelector("#emial").value,
+    receivedemail:document.querySelector("#email").value,
     receivedname:document.querySelector("#name").value,
     receivedpass:document.querySelector("#pass").value
 }
 localStorage.setItem("data",JSON.stringify(obj))
+}
+function login(){
+    let d = JSON.parse(localStorage.getItem("data")) 
+    let newemail = document.querySelector("#email1").value
+    let newpass = document.querySelector("#pass1").value
+    if(newemail===d.receivedemail && newpass===d.receivedpass){
+      window.alert("login sucessfull")
+      window.location.href='./dashboard.html'
+    }
+    else{
+        window.alert("wrong password")
+    }
+    return 0;
 }
